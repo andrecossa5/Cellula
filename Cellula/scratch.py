b9ef7dd654aae1017b382af17a7660fc98ddf616
Cossa Andrea <IEO5505@Cossas-MacBook-Pro.local>
2022-10-21 09:50:51 +0200

    label transfer

--- Cellula/scratch.py
+++ Cellula/scratch.py
@@ -1,40 +1,100 @@
-path_data = path_main + 'data/'
-path_results = path_main + '/results_and_plots/clustering//step_0/'
-path_markers = path_main + '/results_and_plots/dist_features/step_0/'
+path_data = path_main + f'data/{step}/'
+path_results = path_main + f'/results_and_plots/clustering/{step}/'
 
+os.listdir(path_data)
+os.listdir(path_results)
 
-os.listdir(path_markers)
+# Data
+import scanpy as sc
+adata = sc.read(path_data + 'clustered.h5ad')
 
+# scANVi from scARCHEs
 
-adata = sc.read(path_data + 'preprocessed.h5ad')
-top_3 = list(pd.read_excel(path_results + 'summary_results.xlsx', index_col=0).index[:3])
-clustering_solutions = pd.read_csv(
-        path_results + 'clustering_solutions.csv', 
-        index_col=0, 
-        dtype='category'
+# Create mock ref and query
+from sklearn.model_selection import train_test_split 
+idx_ref, idx_query = train_test_split(
+    adata.obs.index, train_size=0.5, random_state=1234
 )
-with open(path_markers + 'clusters_markers.txt', mode='rb') as f:
-    markers = pickle.load(f)
 
-sol = clustering_solutions.loc[:, top_3]
-markers = {
-    k.split('|')[0] : markers[k]['df'] for k in markers if any([ k.split('|')[0] == x for x in top_3 ])
-}
-lognorm = sc.read(path_data + 'lognorm.h5ad')
-lognorm.obs = lognorm.obs.join(sol) # add top3
+a_ref = adata[idx_ref, :][:1000, :]
+a_query = adata[idx_query, :][:1000, :]
 
 
+##
+
+ 
+# scANVI (scarches tutorial)
+import os
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=UserWarning)
+import torch
+import scarches as sca
+from scarches.dataset.trvae.data_handling import remove_sparsity
+import matplotlib.pyplot as plt
+import numpy as np
+import gdown
 
 
+condition_key = 'study'
+cell_type_key = 'cell_type'
+target_conditions = ['Pancreas CelSeq2', 'Pancreas SS2']
+
+url = 'https://drive.google.com/uc?id=1ehxgfHTsMZXy6YzlFKGJOsBKQ5rrvMnd'
+output = 'pancreas.h5ad'
+gdown.download(url, output, quiet=False)
+
+adata_all = sc.read('pancreas.h5ad')
+
+adata = adata_all.raw.to_adata() # Get raw counts
+adata = remove_sparsity(adata) # ???
+adata.X[:1000, :1000]
+
+# Split ref (source) and target (query)
+adata.obs[condition_key].value_counts()
+adata.obs[cell_type_key].value_counts()
+
+source_adata = adata[~adata.obs[condition_key].isin(target_conditions)].copy()
+target_adata = adata[adata.obs[condition_key].isin(target_conditions)].copy()
+source_adata
+target_adata
+
+sca.models.SCVI.setup_anndata(source_adata, batch_key=condition_key, labels_key=cell_type_key)
+
+vae = sca.models.SCVI(
+    source_adata,
+    n_layers=2,
+    encode_covariates=True,
+    deeply_inject_covariates=False,
+    use_layer_norm="both",
+    use_batch_norm="none",
+)
+
+vae.train()
+
+scanvae = sca.models.SCANVI.from_scvi_model(vae, unlabeled_category = "Unknown")
+print("Labelled Indices: ", len(scanvae._labeled_indices))
+print("Unlabelled Indices: ", len(scanvae._unlabeled_indices))
 
 
+scanvae.train(max_epochs=20)
+reference_latent = sc.AnnData(scanvae.get_latent_representation())
+reference_latent.obs["cell_type"] = source_adata.obs[cell_type_key].tolist()
+reference_latent.obs["batch"] = source_adata.obs[condition_key].tolist()
+sc.pp.neighbors(reference_latent, n_neighbors=8)
+sc.tl.leiden(reference_latent)
+sc.tl.umap(reference_latent)
+sc.pl.umap(reference_latent,
+           color=['batch', 'cell_type'],
+           frameon=False,
+           wspace=0.6,
+           )
 
+reference_latent.obs['predictions'] = scanvae.predict()
+print("Acc: {}".format(np.mean(reference_latent.obs.predictions == reference_latent.obs.cell_type)))
 
-
-
-
-
-
+ref_path = 'ref_model/'
+scanvae.save(ref_path, overwrite=True)
 
 
 

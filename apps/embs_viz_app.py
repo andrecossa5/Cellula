fde062609d77fb344ff6d0e2dc60051ef751fbba
Andrea Cossa <IEO5505@Cossas-MacBook-Pro.local>
2023-02-09 17:12:42 +0100

    embs_viz refactoring

--- apps/embs_viz_app.py
+++ apps/embs_viz_app.py
@@ -11,24 +11,6 @@ from Cellula.plotting._plotting_base import *
 from Cellula.plotting._plotting import *
 from Cellula.dist_features._signatures import scanpy_score
 
-
-##
-
-
-def load_data(path_main, version):
-    """
-    Load data.
-    """
-    adata = sc.read(path_main + f'/data/{version}/integration.h5ad')
-    adata_log = sc.read(path_main + f'/data/{version}/lognorm.h5ad')
-    adata_clust = sc.read(path_main + f'/data/{version}/clustered.h5ad')
-    cluster_sol = pd.read_csv(
-        path_main + f'/results_and_plots/clustering/{version}/clustering_solutions.csv',
-        sep=',', index_col=0, dtype='category'
-    )
-    return adata, adata_log, adata_clust, cluster_sol
-
-
 ##
 
 
@@ -39,29 +21,13 @@ st.title('Embeddings visualization')
 
 # Load args
 path_main = sys.argv[1]
-version = sys.argv[2]
+
+# Version
+versions = os.listdir(path_main + 'dist_features_objects/')
+version = st.selectbox("Choose version:", versions, index=versions[0])
 
 # Data
-adata, adata_log, adata_clust, cluster_sol = load_data(path_main, version)
-adata.obs['leiden'] = adata_clust.obs['leiden']
-adata.obs = adata.obs.join(cluster_sol)
-
-# Layer selection
-layer = st.selectbox('Choose layer:', list(adata.layers.keys()))
-# Method selection
-method_keys = list(pd.Series([ x.split('|')[1] for x in adata.obsp.keys()]).unique())
-method = st.selectbox("Choose integration method:", method_keys, index=method_keys.index('original'))
-
-# Compute embeddings
-umap = embeddings(
-    adata, 
-    paga_groups='sample', 
-    rep=method, 
-    layer=layer, 
-    umap_only=True, 
-    k=15, 
-    n_components=30
-)
+adata = sc.read(path_main + f'/data/{version}/clustered.h5ad')
 
 
 ##
@@ -80,18 +46,22 @@ if exp_feature:
     gene_l = input_text.split(',')
     if len(gene_l) == 1:
         gene = gene_l[0]
-        x = adata_log.X[:, adata_log.var_names == gene].toarray().flatten()
+        x = adata.X[:, adata.var_names == gene].toarray().flatten()
         adata.obs[gene] = x
         covariate = gene
     else:
-        x = scanpy_score(adata_log, gene_l)
+        x = scanpy_score(adata, gene_l)
         adata.obs['signature'] = x
         covariate = 'signature'
 else:
     covariate = st.selectbox("Enter a single covariate:", adata.obs.columns, index=0)
 
 # Append to umap df
-umap = umap.join(adata.obs)
+umap = pd.DataFrame(
+    adata.obsm['X_umap'], 
+    columns=['UMAP1', 'UMAP2'], 
+    index=adata.obs_names
+).join(adata.obs)
 
 # Queries, faceting?
 facet = st.selectbox('Enter faceting covariate:', [None] + list(adata.obs.columns), index=0)

e9c14f575cf37fd0444ab0d4f92ebf18a0fd4b4d
Andrea Cossa <IEO5505@Cossas-MacBook-Pro.local>
2023-04-20 14:47:07 +0200

    fix set_logger path join

--- Cellula/_utils.py
+++ Cellula/_utils.py
@@ -82,7 +82,7 @@ def set_logger(path_runs, name, mode='w'):
     A function to open a logs.txt file for a certain script, writing its trace at path_main/runs/step/.
     """
     logger = logging.getLogger("Cellula_logs")
-    handler = logging.FileHandler(path_runs + name, mode=mode)
+    handler = logging.FileHandler(os.path.join(path_runs, name), mode=mode)
     handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
     logger.setLevel(logging.INFO)
     logger.addHandler(handler)

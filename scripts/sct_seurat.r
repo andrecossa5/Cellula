7c10bf6937084beb17ba1cf29bcf66241684689a
Andrea Cossa <IEO5505@Cossas-MacBook-Pro.local>
2023-08-10 15:46:19 +0200

    change sct_seurat

--- scripts/sct_seurat.r
+++ scripts/sct_seurat.r
@@ -9,8 +9,8 @@ tmp <- args[1]
 n <- args[2] 
 
 # Read files
-# tmp <- '/Users/IEO5505/Desktop/example_cellula/data/tmp'
-# n <- 5000
+tmp <- '/Users/IEO5505/Desktop/example_cellula/data/tmp'
+n <- 5000
 
 counts <- fread(paste0(tmp, '/counts.csv')) %>% as.data.frame()
 row.names(counts) <- counts[,1]
@@ -22,11 +22,13 @@ seurat <- CreateSeuratObject(counts=counts, meta.data=meta)
 seurat <- SCTransform(
     seurat, 
     vars.to.regress=c("cycle_diff", "nUMIs", "mito_perc"),
-    variable.features.n=n
+    variable.features.n=n,
+    return.only.var.genes=TRUE
 )
 
 # Extract residuals of HVGs and their attributes
 residuals <- seurat@assays$SCT@scale.data %>% t() %>% as.data.frame()
+print(dim(residuals))
 residuals <- residuals %>% mutate(
                 cells=row.names(residuals), 
                 .before=colnames(residuals)[1]
@@ -36,4 +38,3 @@ df_var <- seurat@assays$SCT@SCTModel.list$model1@feature.attributes
 # Write
 fwrite(residuals, paste0(tmp, '/residuals.csv'))
 write.csv(df_var, paste0(tmp, '/df_var.csv'))
-
